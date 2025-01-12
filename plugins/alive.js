470bfab1ba32152637c2edf94e9b5f918388c002
Dark-Robin <sihileld23@gmail.com>
2025-01-13 03:44:43 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,9 +10,6 @@ cmd({
 async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 const config = await readEnv();
-await robin.sendMessage(from,{sticker: { url : "https://github.com/Dark-Robin/Bot-Helper/raw/refs/heads/main/autosticker/alive.webp"},package: 'S_I_H_I_L_E_L'},{ quoted: mek })
-await robin.sendPresenceUpdate('recording', from);
-await robin.sendMessage(from, { audio: { url: "https://github.com/Dark-Robin/Bot-Helper/raw/refs/heads/main/autovoice/Hiruu%20s.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
